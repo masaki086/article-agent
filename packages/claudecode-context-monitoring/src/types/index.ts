d1c4b0ee85fa663346244024573113af8c4cbb81
TAkusawa <109909287+masaki086@users.noreply.github.com>
2025-08-09 09:10:54 +0900

    cleanup: Remove monitoring implementation (moved to separate repo)   - Removed /optimization directory   - Removed /packages/claudecode-context-monitoring   - Monitoring implementation now in separate repository

--- packages/claudecode-context-monitoring/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all types from one place
-export * from '../core/LanguageAnalyzer';
-export * from '../core/CompactDetector';
-export * from '../core/ContextAnalyzer';
-export * from '../utils/Database';
